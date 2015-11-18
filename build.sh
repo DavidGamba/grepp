1781689a045b9e060db37f7086c77bcda813bb35
David Gamba <davidgamba@gmail.com>
2015-11-18 16:02:34 -0700

    Update build file

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
-#!/bin/sh
-go build -ldflags "-X github.com/davidgamba/grepp/semver.BuildMetadata=`git rev-parse HEAD`" grepp.go
+#!/bin/bash
+
+flags=(-ldflags "-X github.com/davidgamba/grepp/semver.BuildMetadata=`git rev-parse HEAD`")
+
+go install "${flags[@]}" grepp.go
